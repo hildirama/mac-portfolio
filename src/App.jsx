bc0084c3f118a3f4e3ee7fbcdc0804f0b225a956
hildirama <hildirama123@gmail.com>
2026-03-26 22:00:12 +0700

    txtimg window

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import {Navbar, Welcome, Dock} from '#components'
-import { Finder, Resume, Safari, Terminal } from '#windows';
+import { Finder, Image, Resume, Safari, Terminal, Text } from '#windows';
 import gsap from 'gsap';
 
 import { Draggable } from 'gsap/Draggable';
@@ -15,6 +15,8 @@ const App = () => {
       <Safari />
       <Resume />
       <Finder />
+      <Text />
+      <Image />
     </main>
   )
 }
